735011b3782fc2d9f82012849bd0313437ca46a9
aziznurulloh8910 <aziznurulloh8@gmail.com>
2024-07-09 15:13:28 +0700

    [CHG] Seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -29,11 +29,63 @@ class DatabaseSeeder extends Seeder
 
         Asset::factory()->count(20)->create();
 
-        Criteria::factory()
-            ->count(5)
-            ->has(SubCriteria::factory()->count(4), 'subCriteria')
-            ->create();
+        static $id = 1;
 
-        // Criteria::factory()->count(5)->create();
+        // Kriteria Kondisi Aset
+        $kondisiAset = Criteria::create([
+            'criteria_code' => 'C' . $id++,
+            'name' => 'Kondisi Aset',
+            'attribute' => 'benefit',
+            'weight' => 4
+        ]);
+        SubCriteria::insert([
+            ['criteria_id' => $kondisiAset->id, 'name' => 'Baik', 'score' => 1],
+            ['criteria_id' => $kondisiAset->id, 'name' => 'Rusak Ringan', 'score' => 2],
+            ['criteria_id' => $kondisiAset->id, 'name' => 'Rusak Berat', 'score' => 3],
+            ['criteria_id' => $kondisiAset->id, 'name' => 'Barang Tidak Ada', 'score' => 4],
+        ]);
+
+        // Kriteria Usia Aset
+        $usiaAset = Criteria::create([
+            'criteria_code' => 'C' . $id++,
+            'name' => 'Usia Aset',
+            'attribute' => 'cost',
+            'weight' => 3
+        ]);
+        SubCriteria::insert([
+            ['criteria_id' => $usiaAset->id, 'name' => 'C2 <= 5 Tahun', 'score' => 1],
+            ['criteria_id' => $usiaAset->id, 'name' => '6 - 10 Tahun', 'score' => 2],
+            ['criteria_id' => $usiaAset->id, 'name' => '11 - 15 Tahun', 'score' => 3],
+            ['criteria_id' => $usiaAset->id, 'name' => '>= 16 Tahun', 'score' => 4],
+        ]);
+
+        // Kriteria Depresiasi Aset
+        $depresiasiAset = Criteria::create([
+            'criteria_code' => 'C' . $id++,
+            'name' => 'Depresiasi Aset',
+            'attribute' => 'cost',
+            'weight' => 2
+        ]);
+        SubCriteria::insert([
+            ['criteria_id' => $depresiasiAset->id, 'name' => 'C3 <= 5%', 'score' => 1],
+            ['criteria_id' => $depresiasiAset->id, 'name' => '5% - 15%', 'score' => 2],
+            ['criteria_id' => $depresiasiAset->id, 'name' => '15% - 25%', 'score' => 3],
+            ['criteria_id' => $depresiasiAset->id, 'name' => '>= 25%', 'score' => 4],
+        ]);
+
+        // Kriteria Masa Manfaat Aset
+        $masaManfaatAset = Criteria::create([
+            'criteria_code' => 'C' . $id++,
+            'name' => 'Masa Manfaat Aset',
+            'attribute' => 'benefit',
+            'weight' => 1
+        ]);
+        SubCriteria::insert([
+            ['criteria_id' => $masaManfaatAset->id, 'name' => 'C4 <= 5 Tahun', 'score' => 1],
+            ['criteria_id' => $masaManfaatAset->id, 'name' => '6 - 10 Tahun', 'score' => 2],
+            ['criteria_id' => $masaManfaatAset->id, 'name' => '11 - 15 Tahun', 'score' => 3],
+            ['criteria_id' => $masaManfaatAset->id, 'name' => '>= 16 Tahun', 'score' => 4],
+        ]);
+
     }
 }
